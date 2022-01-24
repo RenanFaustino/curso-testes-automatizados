9b5708a176283104c89b9b26729e2437f5ba100a
Gustavo Pedralino <gpedralino@gmail.com>
2022-01-24 16:42:49 -0300

    Atividade Aulas 3 e 4: Finalização da lógica e layout do front-end
    Signed-off-by: Gustavo Pedralino <gustavo.pedralino@venturus.org.br>

--- Aula 4/ui-testing/src/App.tsx
+++ Aula 4/ui-testing/src/App.tsx
@@ -1,20 +1,24 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+interface Skill {
+  skill: string;
+  developers: string;
+  technologies: string;
+  roles: string;
+}
 
 const App = () => {
-  interface Skill {
-    skill: string;
-    developers: string;
-    technologies: string;
-    roles: string;
-  }
   
   const [skill, setSkill] = useState<string>();
   const [developers, setDevelopers] = useState<string>();
   const [technologies, setTechnologies] = useState<string>();
   const [roles, setRoles] = useState<string>();
   const [skillsData, setSkillsData] = useState<Skill[]>([]);
+
+  useEffect(() => {
+    loadSkill()
+  }, [])
 
   function loadSkill() {
     fetch('http://localhost:3333/skills')
@@ -24,7 +28,8 @@ const App = () => {
       });
   }
 
-  const onAddSkill = async () => {
+  const onAddSkill = async (event: React.SyntheticEvent<HTMLButtonElement>) => {
+
     const response = await fetch('http://localhost:3333/skills', {
       body: JSON.stringify({
         skill,
@@ -46,43 +51,42 @@ const App = () => {
   
   return (
     <div className="App">
-        <header className="App-header">
-          <h1>
-            Developers App
-          </h1>
-        </header>
+      <header className="App-header">
+        <h1>
+          Developers App
+        </h1>
+      </header>
 
-      <label htmlFor="skill">Skill Name</label>
-      <input id="skill" className='input1' onChange={(e) => setSkill(e.target.value)} />
+      <div className='container'>
+        <div className="form">
+          <div className='grid'>
+            <label htmlFor="skill">Skill Name</label>
+            <label htmlFor="developers">Developers</label>
+            <label htmlFor="technologies">Technologies</label>
+            <label htmlFor="roles">Roles</label>
+          </div>
 
-      <label htmlFor="developers">Developers</label>
-      <input id="developers" className='input1' onChange={(e) => setDevelopers(e.target.value)} />
-
-      <label htmlFor="technologies">Technologies</label>
-      <input id="technologies" className='input1' onChange={(e) => setTechnologies(e.target.value)} />
-
-      <label htmlFor="roles">Roles</label>
-      <input id="roles" className='input1' onChange={(e) => setRoles(e.target.value)} />
-      
-      <a id="add-button" className="button1" onClick={onAddSkill}>Add Skill</a>
-      <div>
-        <div>
-          <div>Skill Name</div>
-          <div>Developers</div>
-          <div>Technologies</div>
-          <div>Roles</div>
+          <div className='grid'>
+            <input id="skill" className='input1' onChange={(e) => setSkill(e.target.value)} />
+            <input id="developers" className='input1' onChange={(e) => setDevelopers(e.target.value)} />
+            <input id="technologies" className='input1' onChange={(e) => setTechnologies(e.target.value)} />
+            <input id="roles" className='input1' onChange={(e) => setRoles(e.target.value)} />
+            <button type="submit" id="add-button" onClick={onAddSkill} className="button1">Add Skill</button>
+          </div>
+          
         </div>
+
         {skillsData.map((item, index) => (
-        <div key={index}>
-          <div>{item.skill}</div>
-          <div>{item.developers}</div>
-          <div>{item.technologies}</div>
-          <div>{item.roles}</div>
-        </div>
+          <div className='grid data' key={index}>
+            <div>{item.skill}</div>
+            <div>{item.developers}</div>
+            <div>{item.technologies}</div>
+            <div>{item.roles}</div>
+          </div>
         ))}
       </div>
     </div>
-    );
+  );
 }
 
 export default App;
