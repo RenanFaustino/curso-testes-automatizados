f9cb6807126a4ac9c67b9448c8e6d2930df181dc
Gabriel Aparecido Theodoro Oliveira <gabriel.oliveira@venturus.org.br>
2022-01-28 17:54:56 -0300

    Atividade Aulas 3 e 4: Mudança de diretório
    Signed-off-by: Gabriel Oliveira <gabriel.oliveira@venturus.org.br>

---
+++ Aula 4/atividade2/src/App.tsx
@@ -0,0 +1,76 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import './App.css';
+import { useEffect, useState } from 'react';
+interface Skill {
+  skill: string;
+  developers: string;
+  technologies: string;
+  roles: string;
+}
+
+const App = () => {
+  
+  const [skill, setSkill] = useState<string>();
+  const [developers, setDevelopers] = useState<string>();
+  const [technologies, setTechnologies] = useState<string>();
+  const [roles, setRoles] = useState<string>();
+  const [skillsData, setSkillsData] = useState<Skill[]>([]);
+
+  useEffect(() => {
+    loadSkill()
+  }, [])
+
+  //https://61e4d942595afe00176e51cb.mockapi.io/api/v1/skills
+  function loadSkill() {
+    fetch('https://61e4d942595afe00176e51cb.mockapi.io/api/v1/skills')
+        .then(response => response.json())
+      .then(data => {
+        setSkillsData(data);
+      });
+  }
+
+  const onAddSkill = async (event: React.SyntheticEvent<HTMLButtonElement>) => {
+      loadSkill();
+  }
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Developers App
+        </h1>
+      </header>
+
+      <div className='container'>
+        <div className="form">
+          <div className='grid'>
+            <label htmlFor="skill">Skill Name</label>
+            <label htmlFor="developers">Developers</label>
+            <label htmlFor="technologies">Technologies</label>
+            <label htmlFor="roles">Roles</label>
+          </div>
+
+          <div className='grid'>
+            <input id="skill" className='input1' onChange={(e) => setSkill(e.target.value)} />
+            <input id="developers" className='input1' onChange={(e) => setDevelopers(e.target.value)} />
+            <input id="technologies" className='input1' onChange={(e) => setTechnologies(e.target.value)} />
+            <input id="roles" className='input1' onChange={(e) => setRoles(e.target.value)} />
+            <button type="submit" id="add-button" onClick={onAddSkill} className="button1">Add Skill</button>
+          </div>
+          
+        </div>
+
+        {skillsData.map((item, index) => (
+          <div className='grid data' key={index}>
+            <div>{item.skill}</div>
+            <div>{item.developers}</div>
+            <div>{item.technologies}</div>
+            <div>{item.roles}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- Aula 4/ui-testing/src/App.tsx
+++ Aula 4/ui-testing/src/App.tsx
@@ -1,92 +1,97 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
-import { useEffect, useState } from 'react';
-interface Skill {
-  skill: string;
-  developers: string;
-  technologies: string;
-  roles: string;
-}
+import { useState } from 'react';
 
 const App = () => {
+
+  const [skillData, setSkillData] = useState({
+    "skillName": "",
+    "developers": [],
+    "profile": {
+      "technologies": [],
+      "roles": []
+    }
+  });
+  const [skillsData, setSkillsData] = useState([{
+    "skillName": "",
+    "developers": [],
+    "profile": {
+      "technologies": [],
+      "roles": []
+    }
+  }]);
+
+  const [showSkill, setShowSkill] = useState(false);
+  const [showSkills, setShowSkills] = useState(false);
+
+  let listSkills = skillsData.map((skill, index) =>
+      <ul key={index}>
+        <li>Skill Name: {skill.skillName}</li>
+        <li>Developers:
+          <ul>
+          <li>{skill.developers[0]}</li>
+          <li>{skill.developers[1]}</li>
+          </ul>
+        </li>
+        <li>Technologies:
+          <ul>
+          <li>{skill.profile.technologies[0]}</li>
+          <li>{skill.profile.technologies[1]}</li>
+          </ul>
+        </li>
+      </ul>);
+
+  let name = <><h3>{skillData.skillName}</h3>
+    <ul>
+    <li>Skill Name: {skillData.skillName}</li>
+    <li>Developers:
+      <ul>
+        <li>{skillData.developers[0]}</li>
+        <li>{skillData.developers[1]}</li>
+      </ul>
+    </li>
+    <li>Technologies:
+      <ul>
+        <li>{skillData.profile.technologies[0]}</li>
+        <li>{skillData.profile.technologies[1]}</li>
+      </ul>
+    </li>
+  </ul></>;
   
-  const [skill, setSkill] = useState<string>();
-  const [developers, setDevelopers] = useState<string>();
-  const [technologies, setTechnologies] = useState<string>();
-  const [roles, setRoles] = useState<string>();
-  const [skillsData, setSkillsData] = useState<Skill[]>([]);
-
-  useEffect(() => {
-    loadSkill()
-  }, [])
-
   function loadSkill() {
-    fetch('http://localhost:3333/skills')
+    setShowSkill(true);
+    setShowSkills(false);
+    fetch('https://61e4d942595afe00176e51cb.mockapi.io/api/v1/skill')
+        .then(response => response.json())
+      .then(data => {
+        setSkillData(data[0]);
+        });
+  }
+  function loadSkills() {
+    setShowSkill(false);
+    setShowSkills(true);
+    fetch('https://61e4d942595afe00176e51cb.mockapi.io/api/v1/skills')
         .then(response => response.json())
       .then(data => {
         setSkillsData(data);
-      });
+        });
   }
 
-  const onAddSkill = async (event: React.SyntheticEvent<HTMLButtonElement>) => {
-
-    const response = await fetch('http://localhost:3333/skills', {
-      body: JSON.stringify({
-        skill,
-        developers,
-        technologies,
-        roles
-      }),
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-    });
-
-    if (response.ok) {
-      loadSkill();
-    }
-  }
-  
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Developers App
-        </h1>
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Developers App
+          </h1>
       </header>
-
-      <div className='container'>
-        <div className="form">
-          <div className='grid'>
-            <label htmlFor="skill">Skill Name</label>
-            <label htmlFor="developers">Developers</label>
-            <label htmlFor="technologies">Technologies</label>
-            <label htmlFor="roles">Roles</label>
-          </div>
-
-          <div className='grid'>
-            <input id="skill" className='input1' onChange={(e) => setSkill(e.target.value)} />
-            <input id="developers" className='input1' onChange={(e) => setDevelopers(e.target.value)} />
-            <input id="technologies" className='input1' onChange={(e) => setTechnologies(e.target.value)} />
-            <input id="roles" className='input1' onChange={(e) => setRoles(e.target.value)} />
-            <button type="submit" id="add-button" onClick={onAddSkill} className="button1">Add Skill</button>
-          </div>
-          
-        </div>
-
-        {skillsData.map((item, index) => (
-          <div className='grid data' key={index}>
-            <div>{item.skill}</div>
-            <div>{item.developers}</div>
-            <div>{item.technologies}</div>
-            <div>{item.roles}</div>
-          </div>
-        ))}
-      </div>
+      <label htmlFor="input1">Type the skill you would like to search</label>
+        <input id="input-skill" className='input1'></input>
+      <a id="search-button" className="button1" onClick={() => loadSkill()}>Search</a>
+        <a id="load-skills-button" className="button1" onClick={() => loadSkills()}>All Skills</a>
+          {showSkill? name : ''}
+          {showSkills? listSkills: ''}
     </div>
-  );
+    );
 }
 
 export default App;
